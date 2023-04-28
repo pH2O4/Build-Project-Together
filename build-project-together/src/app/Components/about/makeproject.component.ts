c5aa7935e720706d9d4f74da9be0d2d154132892
pH2O4 <ph2oalencar@gmail.com>
2023-04-28 00:07:06 -0300

    Limit selection

--- build-project-together/src/app/Components/about/makeproject.component.ts
+++ build-project-together/src/app/Components/about/makeproject.component.ts
@@ -1,40 +1,53 @@
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import {Component, ElementRef, ViewChild} from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
-import {MatChipInputEvent} from '@angular/material/chips';
-import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
-import {Task} from '../../Interfaces/Task'
-import { faFaceSmile, faFaceTired, faFaceAngry } from '@fortawesome/free-solid-svg-icons';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { Task } from '../../Interfaces/Task';
+import {
+  faFaceSmile,
+  faFaceTired,
+  faFaceAngry,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'make-project',
   templateUrl: './makeproject.component.html',
-  styleUrls: ['./makeproject.component.css']
+  styleUrls: ['./makeproject.component.css'],
 })
 export class MakeProjectComponent {
-  faFaceSmile = faFaceSmile
-  faFaceTired = faFaceTired
-  faFaceAngry = faFaceAngry
+  faFaceSmile = faFaceSmile;
+  faFaceTired = faFaceTired;
+  faFaceAngry = faFaceAngry;
   separatorKeysCodes: number[] = [ENTER, COMMA];
   fruitCtrl = new FormControl('');
   filteredFruits: Observable<string[]>;
   fruits: string[] = ['Social Media'];
-  allFruits: string[] = ['Gerente de RH', 'Gestore de Tráfego', 'Contador', 'DevOps', 'Desenvolvedor Java'];
+  allFruits: string[] = [
+    'Gerente de RH',
+    'Gestore de Tráfego',
+    'Contador',
+    'DevOps',
+    'Desenvolvedor Java',
+    'Social Midia'
+  ];
   myOptions = [
     { label: 'Saúde', value: 'Saúde' },
     { label: 'Marketing Digital', value: 'Marketing Digital' },
-    { label: 'TI', value: 'TI' }
+    { label: 'TI', value: 'TI' },
   ];
-  selectedOption = ''
+  selectedOption = '';
 
   @ViewChild('fruitInput') fruitInput!: ElementRef<HTMLInputElement>;
 
   constructor() {
     this.filteredFruits = this.fruitCtrl.valueChanges.pipe(
       startWith(null),
-      map((fruit: string | null) => (fruit ? this._filter(fruit) : this.allFruits.slice())),
+      map((fruit: string | null) =>
+        fruit ? this._filter(fruit) : this.allFruits.slice()
+      )
     );
   }
 
@@ -54,6 +67,7 @@ export class MakeProjectComponent {
 
   remove(fruit: string): void {
     const index = this.fruits.indexOf(fruit);
+    console.log('teste');
 
     if (index >= 0) {
       this.fruits.splice(index, 1);
@@ -61,7 +75,9 @@ export class MakeProjectComponent {
   }
 
   selected(event: MatAutocompleteSelectedEvent): void {
-    this.fruits.push(event.option.viewValue);
+    if (this.fruits.length <= 2) {
+      this.fruits.push(event.option.viewValue);
+    }
     this.fruitInput.nativeElement.value = '';
     this.fruitCtrl.setValue(null);
   }
@@ -69,7 +85,9 @@ export class MakeProjectComponent {
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
 
-    return this.allFruits.filter(fruit => fruit.toLowerCase().includes(filterValue));
+    return this.allFruits.filter((fruit) =>
+      fruit.toLowerCase().includes(filterValue)
+    );
   }
 
   task: Task = {
@@ -77,24 +95,34 @@ export class MakeProjectComponent {
     completed: false,
     color: 'primary',
     subtasks: [
-      {name: 'Remoto', completed: false, color: 'primary', icon: faFaceSmile},
-      {name: 'Hibrido', completed: false, color: 'accent', icon: faFaceTired},
-      {name: 'Presencial', completed: false, color: 'warn', icon: faFaceAngry},
+      { name: 'Remoto', completed: false, color: 'primary', icon: faFaceSmile },
+      { name: 'Hibrido', completed: false, color: 'accent', icon: faFaceTired },
+      {
+        name: 'Presencial',
+        completed: false,
+        color: 'warn',
+        icon: faFaceAngry,
+      },
     ],
-    icon: faFaceSmile
+    icon: faFaceSmile,
   };
 
   allComplete: boolean = false;
 
   updateAllComplete() {
-    this.allComplete = this.task.subtasks != null && this.task.subtasks.every(t => t.completed);
+    this.allComplete =
+      this.task.subtasks != null &&
+      this.task.subtasks.every((t) => t.completed);
   }
 
   someComplete(): boolean {
     if (this.task.subtasks == null) {
       return false;
     }
-    return this.task.subtasks.filter(t => t.completed).length > 0 && !this.allComplete;
+    return (
+      this.task.subtasks.filter((t) => t.completed).length > 0 &&
+      !this.allComplete
+    );
   }
 
   setAll(completed: boolean) {
@@ -102,6 +130,10 @@ export class MakeProjectComponent {
     if (this.task.subtasks == null) {
       return;
     }
-    this.task.subtasks.forEach(t => (t.completed = completed));
+    this.task.subtasks.forEach((t) => (t.completed = completed));
+  }
+
+  teste(): void {
+    console.log('TESTE');
   }
 }
